044cb966066ef51de05e10df982bc7806489d412
Jose Carlos <josec.quichiz@gmail.com>
2016-12-06 21:17:26 -0500

    Register page finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
-    var btnInscribete = $("button.btnInscribete");
+    var btnInscribete = $("button.btnInscribete"),
+        selectBox = $("button#selectBox"),
+        selectBoxText = $("button#selectBox").children("span"),
+        menuBox = selectBox.next(),
+        regForm = $("#form-container > form"),
+        imgConfirmForm = $("#form-container > form").next();
     btnInscribete.click(function(){
         location.href = "registro.html";
-    })
+    });
+    selectBox.click(function(e){
+        e.preventDefault();
+        menuBox.toggle("fast");
+    });
+    menuBox.find("label").click(function(e){
+        e.preventDefault();
+        selectBoxText.text($(this).children("span").text());
+        menuBox.fadeOut("fast");
+    });
+    regForm.submit(function(e){
+        e.preventDefault();
+        regForm.slideUp("fast");
+        imgConfirmForm.fadeIn("fast");
+    });
 });
